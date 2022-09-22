17d0fc257d5321de5ea3a846f77fe94ab4dfed49
Attidd <ballotdeath@gmail.com>
2022-09-22 17:30:15 -0400

    Added files for the CSS & HTML section

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
 import { RouterLink, RouterView } from "vue-router";
 import LeftVue from "./components/LeftVue.vue";
 import VueBlock from "./components/VueBlock.vue";
+import StylingBlock from "./components/StylingBlock.vue";
 </script>
 
 <template>
+<StylingBlock/>
 <VueBlock/>
+
 
 
 </template>
