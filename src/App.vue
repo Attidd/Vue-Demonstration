c09b0b6ea8aa8b58af9b690d788025217150626c
Attidd <ballotdeath@gmail.com>
2022-09-22 17:33:30 -0400

    Added file for Footer

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@ import { RouterLink, RouterView } from "vue-router";
 import LeftVue from "./components/LeftVue.vue";
 import VueBlock from "./components/VueBlock.vue";
 import StylingBlock from "./components/StylingBlock.vue";
+import FooterBlock from "./components/FooterBlock.vue";
 </script>
 
 <template>
 <StylingBlock/>
 <VueBlock/>
-
+<FooterBlock/>
 
 
 </template>
